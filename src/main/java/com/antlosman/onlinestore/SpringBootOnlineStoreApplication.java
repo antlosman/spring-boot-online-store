dfa29402f3500b5c2f09c3f2d14687bc6d14f9cc
Anton Losman <87125898+antlosman@users.noreply.github.com>
2022-10-25 09:57:16 +0300

    Initial commit

---
+++ src/main/java/com/antlosman/onlinestore/SpringBootOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package com.antlosman.onlinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOnlineStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antlosman/onlinestore/SpringBootOnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antlosman.onlinestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
