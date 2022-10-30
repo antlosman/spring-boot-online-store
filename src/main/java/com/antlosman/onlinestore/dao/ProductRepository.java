e05123d399e0a9a8edd4b66379c7036c760a3214
Anton Losman <87125898+antlosman@users.noreply.github.com>
2022-10-30 11:37:54 +0200

    [add] - search product by category backend added

--- src/main/java/com/antlosman/onlinestore/dao/ProductRepository.java
+++ src/main/java/com/antlosman/onlinestore/dao/ProductRepository.java
@@ -1,7 +1,14 @@
 package com.antlosman.onlinestore.dao;
 
 import com.antlosman.onlinestore.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    // select * from product where category_id=?
+    // Spring Data REST automatically exposes endpoint http://localhost:8080/products/search/findByCategory?id=someId
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
