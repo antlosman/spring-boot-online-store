8b9fec5718a010bd00677eb38485413d20254a1b
Anton Losman <87125898+antlosman@users.noreply.github.com>
2022-11-08 10:32:25 +0200

    [add] - CheckoutController created

---
+++ src/main/java/com/antlosman/onlinestore/controller/CheckoutController.java
@@ -0,0 +1,28 @@
+package com.antlosman.onlinestore.controller;
+
+import com.antlosman.onlinestore.dto.Purchase;
+import com.antlosman.onlinestore.dto.PurchaseResponse;
+import com.antlosman.onlinestore.service.CheckoutService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/checkout")
+public class CheckoutController {
+
+    private final CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+        return purchaseResponse;
+    }
+}
