7bf185b809adf5abbb555643d9ac792b2907e365
Anton Losman <87125898+antlosman@users.noreply.github.com>
2022-11-27 12:55:59 +0200

    [change] - add order to a customer by customers email

--- src/main/java/com/antlosman/onlinestore/dao/CustomerRepository.java
+++ src/main/java/com/antlosman/onlinestore/dao/CustomerRepository.java
@@ -4,4 +4,7 @@ import com.antlosman.onlinestore.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    // SELECT * FROM Customer c WHERE c.email = customerEmail
+    Customer findByEmail(String customerEmail);
 }
